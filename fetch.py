5ea257b8beb6cda48e938b6a03ed9643369a0bbe
Khaled Sharif <kldsrf@gmail.com>
2024-05-09 11:21:53 -0700

    small fixes

--- fetch.py
+++ fetch.py
@@ -8,14 +8,31 @@ from textwrap import wrap
 from tqdm import tqdm
 
 
-text_maker = html2text.HTML2Text()
-text_maker.ignore_links = True
-text_maker.ignore_images = True
-text_maker.ignore_emphasis = True
-text_maker.ignore_mailto_links = True
-text_maker.ignore_tables = False
-text_maker.bypass_tables = False
-text_maker.body_width = 150
+class Fetcher:
+    def __init__(self) -> None:
+        self.text_maker = html2text.HTML2Text()
+        self.text_maker.ignore_links = True
+        self.text_maker.ignore_images = True
+        self.text_maker.ignore_emphasis = True
+        self.text_maker.ignore_mailto_links = True
+        self.text_maker.ignore_tables = False
+        self.text_maker.bypass_tables = False
+        self.text_maker.body_width = 150
+
+        self.chroma = chromadb.HttpClient(host="localhost", port=8000)
+
+
+def clean_string(input_string):
+    # Remove non-ASCII characters
+    ascii_string = re.sub(r"[^\x00-\x7F]+", "", input_string)
+
+    # Remove extra whitespace
+    clean_string = re.sub(r"\n{2,}", "\n", ascii_string)
+
+    return clean_string
+
+
+collection_name = getconfig()["name"]
 
 
 class ExtractText(dspy.Signature):
@@ -44,88 +61,77 @@ dspy.settings.configure(lm=lm_provider)
 
 gen_chunks = dspy.ChainOfThoughtWithHint(ExtractText)
 
-def generate_chunks():
+
+def generate_chunks(chunk):
     c = gen_chunks(
-        document=chunk, hint="""
+        document=chunk,
+        hint="""
         extract useful text from the document
-        """
+        """,
     )
     return c.text
 
-def clean_string(input_string):
-    # Remove non-ASCII characters
-    ascii_string = re.sub(r"[^\x00-\x7F]+", "", input_string)
 
-    # Remove extra whitespace
-    clean_string = re.sub(r"\n{2,}", "\n", ascii_string)
+if __name__ == "__main__":
+    fetcher = Fetcher()
 
-    return clean_string
+    if any(
+        collection.name == collection_name
+        for collection in fetcher.chroma.list_collections()
+    ):
+        print("deleting collection")
+        fetcher.chroma.delete_collection(collection_name)
 
+    collection = fetcher.chroma.get_or_create_collection(
+        name=collection_name, metadata={"hnsw:space": "cosine"}
+    )
 
-collection_name = getconfig()["name"]
+    embedmodel = getconfig()["embedmodel"]
 
-chroma = chromadb.HttpClient(host="localhost", port=8000)
+    starttime = time.time()
 
-if any(collection.name == collection_name for collection in chroma.list_collections()):
-    print("deleting collection")
-    chroma.delete_collection(collection_name)
+    with open(getconfig()["sources"]) as f:
+        lines = f.readlines()
 
-collection = chroma.get_or_create_collection(
-    name=collection_name, metadata={"hnsw:space": "cosine"}
-)
+    for line_index, filename in enumerate(lines):
+        print("\n", "=" * 40, "\n")
+        print(filename, "\n")
 
-embedmodel = getconfig()["embedmodel"]
-starttime = time.time()
-with open(getconfig()["sources"]) as f:
-    lines = f.readlines()
+        chunks = fetcher.text_maker.handle(readtext(filename).prettify())
 
+        step_size = int(getconfig()["chunksize"])
+        step_overlap = step_size // 10
+        for chunk_index in tqdm(range(0, len(chunks), step_size - step_overlap)):
+            chunk_end = min(len(chunks), chunk_index + step_size)
 
-for line_index, filename in enumerate(lines):
-    print("\n", "=" * 40, "\n")
-    print(filename, "\n")
+            chunk = chunks[chunk_index:chunk_end]
+            chunk = clean_string(chunk.strip()).strip()
 
-    content_path = get_filename(filename)
+            if len(chunk) < 1_000:
+                print("chunk too small, skipping")
+                continue
 
-    soup = readtext(filename).prettify()
+            chunk_processed = generate_chunks(chunk)
 
-    chunks = text_maker.handle(soup)
+            chunk_processed = "\n".join(wrap(chunk_processed, width=150))
 
-    step_size = int(getconfig()["chunksize"])
-    step_overlap = step_size // 10
-    for chunk_index in tqdm(range(0, len(chunks), step_size - step_overlap)):
-        chunk_end = min(len(chunks), chunk_index + step_size)
+            with open(
+                f"sentences/line_{line_index}_step_{step_size}_chunk_{chunk_index}.txt",
+                "w",
+            ) as senf:
+                senf.write("=" * 40 + "\n")
+                senf.write("INPUT" + "\n")
+                senf.write("=" * 40 + "\n")
+                senf.write(chunk)
 
-        chunk = chunks[chunk_index:chunk_end]
-        chunk = clean_string(chunk.strip()).strip()
+                senf.write("\n\n")
 
-        if len(chunk) < 1_000:
-            continue
+                senf.write("=" * 40 + "\n")
+                senf.write("OUTPUT" + "\n")
+                senf.write("=" * 40 + "\n")
+                senf.write(chunk_processed)
 
-        try:
-
-        except TypeError as e:
-            print("warning type error during dspy extraction")
-            continue
-
-        chunk_processed = "\n".join(wrap(chunk_processed, width=150))
-
-        with open(
-            f"sentences/line_{line_index}_step_{step_size}_chunk_{chunk_index}.txt",
-            "w",
-        ) as senf:
-            senf.write("=" * 40 + "\n")
-            senf.write("INPUT" + "\n")
-            senf.write("=" * 40 + "\n")
-            senf.write(chunk)
-
-            senf.write("\n\n")
-
-            senf.write("=" * 40 + "\n")
-            senf.write("OUTPUT" + "\n")
-            senf.write("=" * 40 + "\n")
-            senf.write(chunk_processed)
-
-            senf.write("\n\n")
+                senf.write("\n\n")
 
         embed = ollama.embeddings(model=embedmodel, prompt=chunk_processed)["embedding"]
         collection.add(
@@ -135,4 +141,4 @@ for line_index, filename in enumerate(lines):
             metadatas={"source": filename},
         )
 
-print("--- %s seconds ---" % (time.time() - starttime))
+    print("--- %s seconds ---" % (time.time() - starttime))
